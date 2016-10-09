bee9c8825dca44e49f48505888d0fdd9f65d204a
Constantinus <asconst@yandex.ru>
2016-10-09 13:10:02 +0300

    First ver

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>
+			Cs' home
+		</title>
+	</head>
+	<body>
+	
+<!--	SIDE PANEL  -->
+	
+		<div class="side">
+	<!-- LOGO  -->
+		<div class="logo">
+			C's Home
+		</div>
+	<!-- MENU -->	
+		<div class="menu">
+			<div class="href"><a class="amenu">1</a></div>
+			<div class="href"><a class="amenu">2</a></div>
+			<div class="href"><a class="amenu">3</a></div>
+			<div class="href"><a class="amenu">4</a></div>
+			<div class="href"><a class="amenu">5</a></div>
+		</div>
+	<!-- FOOTER -->	
+		<footer class="footer">
+			<p>
+				(C)Cs 2016
+			</p>
+		</footer>
+		</div>
+	<!-- CONTENT -->	
+		<div class="content">
+			<?php
+				require('main.php')
+			?>				
+		</div>
+		
+<!--  SIDE PANEL END  -->		
+		
+<link rel="stylesheet" type="text/css" href="style.css">
+<link rel="stylesheet" type="text/css" href="design.css">
+	</body>
+</html>
